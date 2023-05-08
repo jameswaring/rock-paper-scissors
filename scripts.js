47db0113a09810b7bdf03cb837d696969ce8b7af
James Waring <jameswaring@WAS-C02YD182JGH5.local>
2023-05-08 16:18:41 -0400

    basic functionality complete

--- scripts.js
+++ scripts.js
@@ -1,8 +1,31 @@
-function getComputerChoice(){
+function getComputerChoice(userChoice){
     items = ["rock.png", "paper.png", "scissors.png"]
     let randomSelection = items[Math.floor(Math.random()*items.length)];
     const compPic = document.getElementById("compPic")
     compPic.src = randomSelection
+    strippedSelection = randomSelection.replace(/\.[^/.]+$/, "")
+    console.log(strippedSelection)
+    if(strippedSelection == userChoice){
+        tie()
+    }
+    else if(strippedSelection == "rock" && userChoice == "paper"){
+        win()
+    }
+    else if(strippedSelection == "paper" && userChoice == "rock"){
+        lose()
+    }
+    else if(strippedSelection == "scissors" && userChoice == "paper"){
+        lose()
+    }
+    else if(strippedSelection == "scissors" && userChoice == "rock"){
+        win()
+    }
+    else if(strippedSelection == "rock" && userChoice == "scissors"){
+        lose()
+    }
+    else if(strippedSelection == "paper" && userChoice == "rock"){
+        lose()
+    }
 }
 
 function rockSelect(){
@@ -27,5 +50,16 @@ function paperSelect(){
     const scissors = document.getElementById("scissors")
     scissors.classList.add("opaque")
     getComputerChoice("paper")
-    
+}
+
+function win(){
+    document.getElementById("thisround").innerHTML = "WIN!"
+}
+
+function lose(){
+    document.getElementById("thisround").innerHTML = "LOSE!"
+}
+
+function tie(){
+    document.getElementById("thisround").innerHTML = "TIE!"
 }
